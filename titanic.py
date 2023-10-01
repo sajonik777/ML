c4bdc64b5ab38e6141aef93e510f3aa5a60824b5
John Hill <143519258+sajonik777@users.noreply.github.com>
2023-10-02 00:25:56 +0300

    Added code to visualize the most important patterns for the RandomForestClassifier model as a barplot, but encountered an error "name 'plt' is not defined".

--- titanic.py
+++ titanic.py
@@ -1,5 +1,6 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
 
 import os
 for dirname, _, filenames in os.walk('/kaggle/input'):
@@ -14,7 +15,7 @@ train_data.fillna(0, inplace=True)
 # Exclude rows with 'Age' value less than '10'
 train_data = train_data[train_data.Age >= 10]
 
-train_data.describe()
+train_data.head()
 
 test_data = pd.read_csv("/kaggle/input/titanic/test.csv")
 
@@ -44,12 +45,25 @@ features = ["Pclass", "Sex", "Age", "SibSp", "Parch"]
 X = pd.get_dummies(train_data[features])
 X_test = pd.get_dummies(test_data[features])
 
-model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model = RandomForestClassifier(n_estimators=50, max_depth=20, random_state=1)
 model.fit(X, y)
 predictions = model.predict(X_test)
 
-print(round(model.score(X, y), 2))
+print(round(model.score(X, y), 3))
+#output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+#output.to_csv('submission.csv', index=False)
 
-# output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
-# output.to_csv('submission.csv', index=False)
-# print("Your submission was successfully saved!")
+#print("Your submission was successfully saved!")
+
+# Extract feature importances
+importances = model.feature_importances_
+feature_importances = pd.DataFrame({'feature': X.columns, 'importance': importances})
+feature_importances = feature_importances.sort_values('importance', ascending=False)
+
+# Plot feature importances
+plt.figure(figsize=(8, 4))
+plt.bar(feature_importances['feature'], feature_importances['importance'])
+plt.xlabel('Features')
+plt.ylabel('Importance')
+plt.title('Feature importances from RandomForestClassifier')
+plt.show()
