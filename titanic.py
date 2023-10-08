e7249cdc12b3e7f61341f5a3f1ccb7bf85eb0ff2
John Hill <143519258+sajonik777@users.noreply.github.com>
2023-10-08 23:26:20 +0300

    Create file "Titanic2" in the Kaggle path and add a script for feature engineering, data preprocessing, and model training using a RandomForestClassifier.

--- titanic.py
+++ titanic.py
@@ -1,124 +1,212 @@
-import numpy as np
-import pandas as pd
-import os
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.ensemble import RandomForestClassifier
-import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import GridSearchCV, KFold
+from sklearn.neighbors import KNeighborsRegressor
+from scipy import stats
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 
+from sklearn.metrics import mean_absolute_error
+# We take the average of absolute errors. 
+# This is our measure of model quality 
+from sklearn.model_selection import GridSearchCV, KFold
+
+
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
 for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
+    
+
+# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
 
 train_data = pd.read_csv("/kaggle/input/titanic/train.csv")
-median_ages_train = train_data.groupby(['Sex', 'Pclass'])['Age'].median()
-train_data.fillna(0, inplace=True)
-train_data.loc[train_data.Age == 0, 'Age'] = train_data[train_data.Age == 0].apply(lambda row: median_ages_train[row['Sex'], row['Pclass']], axis=1)
-train_data = train_data[train_data.Age >= 10]
-train_data.describe()
+
+# Feature Engineering
+# Update the 'Embarked' column for passengers with ID "830" and "62"
+train_data.loc[train_data['PassengerId'].isin([830, 62]), 'Embarked'] = 'S'
+
+# Drop rows where 'Age' is NaN
+train_data.dropna(subset=['Age'], inplace=True)
+
+# Add condition, if male traveled alone then survived=0
+Alone = (train_data['SibSp'] + train_data['Parch'] == 0) & (train_data['Sex'] == 'male') & (train_data['Age'] > 17)
+train_data.loc[Alone, 'Survived'] = 0
+train_data['Alone'] = Alone.astype(int)
+
+# if female from 17 to 65 then survived=1
+Women = (train_data['Sex'] == 'female') & ((train_data['Age'] >= 17) & (train_data['Age'] <= 65))
+train_data.loc[Women, 'Survived'] = 1
+train_data['Women'] = Women.astype(int)
+
+# if children (both male and female) from 5 to 17 then survived=1
+Children = ((train_data['Age'] >= 3) & (train_data['Age'] <= 15))
+train_data.loc[Children, 'Survived'] = 1
+train_data['Children'] = Children.astype(int)
+
+train_data.head()
+
+train_data.shape
 
 test_data = pd.read_csv("/kaggle/input/titanic/test.csv")
-median_ages_test = test_data.groupby(['Sex', 'Pclass'])['Age'].median()
+
+# Feature Engineering
+average_ages_test = test_data.groupby(['Sex', "Parch"])['Age'].mean()
+test_data.loc[test_data.Age == 0, 'Age'] = test_data[test_data.Age == 0].apply(lambda row: average_ages_test[row['Sex'], row['Parch']], axis=1)
+
+# if female from 17 to 65 then survived=1
+Women = (test_data['Sex'] == 'female') & ((test_data['Age'] >= 17) & (test_data['Age'] <= 65))
+test_data.loc[Women, 'Survived'] = 1
+test_data['Women'] = Women.astype(int)
+
+# if children (both male and female) from 5 to 17 then survived=1
+Children = ((test_data['Age'] >= 3) & (test_data['Age'] <= 15))
+test_data.loc[Children, 'Survived'] = 1
+test_data['Children'] = Children.astype(int)
+
+# Drop rows where 'Age' is NaN
+#test_data.dropna(subset=['Age'], inplace=True)
+
+# Add condition, if male traveled alone then survived=0
+Alone = (test_data['SibSp'] + test_data['Parch'] == 0) & (test_data['Sex'] == 'male')
+test_data['Alone'] = Alone.astype(int)
+
 test_data.fillna(0, inplace=True)
-test_data.loc[test_data.Age == 0, 'Age'] = test_data[test_data.Age == 0].apply(lambda row: median_ages_test[row['Sex'], row['Pclass']], axis=1)
-test_data = test_data[test_data.Age >= 10]
+
 test_data.describe()
 
 women = train_data.loc[train_data.Sex == 'female']["Survived"]
 rate_women = sum(women)/len(women)
+
 print("% of women who survived:", rate_women)
+
+women = train_data.loc[train_data.Sex == 'female']["Survived"]
+rate_women = sum(women)/len(women)
+
+print("% of women who survived:", round((rate_women), 3))
+
+# Calculate survival rate for children (age 1-17)
+children = train_data.loc[(train_data.Age >= 1) & (train_data.Age <= 17)]["Survived"]
+rate_children = sum(children)/len(children)
+
+print("% of children who survived:", round((rate_children), 3))
+
+# Summarize the survival rates
+print("% of women and children who survived:", round((rate_women + rate_children) / 2, 3))
 
 men = train_data.loc[train_data.Sex == 'male']["Survived"]
 rate_men = sum(men)/len(men)
+
 print("% of men who survived:", rate_men)
 
 y = train_data["Survived"]
-features = ["Pclass", "Sex", "Age", "SibSp", "Parch"]
-X = pd.get_dummies(train_data[features])
+
+features = ["Women", "Children", "Sex"]
+X_train = pd.get_dummies(train_data[features])
 X_test = pd.get_dummies(test_data[features])
 
-# Standardize the features
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
-
 # Apply PCA
-pca = PCA(n_components=30)
-principalComponents = pca.fit_transform(X_scaled)
+#pca = PCA(n_components=4)
+#principalComponents = pca.fit_transform(X_train)
+#X_train_pca = pca.transform(X_train)
+#X_test_pca = pca.transform(X_test)
+
+model = RandomForestClassifier(n_estimators=10, max_depth=5, random_state=0)
+model.fit(X_train, y)
+predictions = model.predict(X_test)
+
+print(round(model.score(X_train, y), 3))
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('submission.csv', index=False)
+
+print("Your submission was successfully saved!")
+
+from sklearn.model_selection import train_test_split
+
+# split data into training and validation data, for both features and target
+# The split is based on a random number generator. Supplying a numeric value to
+# the random_state argument guarantees we get the same split every time we
+
+train_X, val_X, train_y, val_y = train_test_split(X_train, y, random_state = 0)
+# Define model
+model_dtr = RandomForestClassifier()
+# Fit model
+model_dtr.fit(train_X, train_y)
+
+# get predicted on validation data
+val_predictions = model_dtr.predict(val_X)
+
+print("MAE for out-of-sample data is:")
+
+mae = mean_absolute_error(val_y, val_predictions)
+print(round(mae, 4))
+
+
+# define get_mae function that takes the maximum number of leaf nodes for dtr
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    
+# define training and validation data for features and target    
+    model_leaf = RandomForestClassifier(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    
+# fit the model to the training data    
+    model_leaf.fit(train_X, train_y)
+    
+# makes predictions on the validation data    
+    preds_val = model_leaf.predict(val_X)
+    
+# calculates the mean absolute error    
+    mae_leaf = mean_absolute_error(val_y, preds_val)
+    
+    return(mae_leaf)
+
+# compare MAE with differing values of max_leaf_nodes
+for max_leaf_nodes in [5, 10, 50, 500]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %.5f" %(max_leaf_nodes, round(my_mae, 5)))
+
+ # Define a parameter grid for the RandomForestClassifier
+param_grid = {
+     'n_estimators': [5, 10, 50, 100],
+     'max_depth': [5, 10, 15, 20, None],
+     'random_state': [0]
+ }
+
+# Initialize a GridSearchCV object with the RandomForestClassifier and the parameter grid
+grid_search = GridSearchCV(RandomForestClassifier(), param_grid, cv=5, scoring='accuracy')
+
+# Fit the GridSearchCV object to the training data
+grid_search.fit(X_train, y)
+
+# Print out the best parameters found by GridSearchCV
+print("Best parameters found by GridSearchCV:", grid_search.best_params_)
 
 # Get the explained variance ratio
 explained_variance_ratio = pca.explained_variance_ratio_
-print("Explained Variance Ratio: ", explained_variance_ratio)
-
-# Create labels for PCA components
-labels = ['PC' + str(x) for x in range(1, len(pca.components_) + 1)]
-
-# Create a DataFrame from the PCA components
-df_pca = pd.DataFrame(pca.components_, columns=labels, index=X.columns)
-
-# Display the transpose of the first 10 rows of the DataFrame
-print(df_pca.head(10).round(5).T)
 
 # Set seaborn style
 sns.set(style='whitegrid')
-
+ 
 # Plot the cumulative sum of the explained variance ratio
 plt.plot(np.cumsum(explained_variance_ratio))
 plt.xlabel('number of components')
 plt.ylabel('cumulative explained variance')
-
+ 
 # Add a vertical dashed red line at x=10
-plt.axvline(linewidth=4, color='r', linestyle='--', x=10, ymin=0, ymax=1)
-
+plt.axvline(linewidth=2, color='r', linestyle = '--', x=5, ymin=0, ymax=1)
+ 
 # Display the plot
 plt.show()
-
-# Number of trees in random forest
-n_estimators = [int(x) for x in np.linspace(start = 100, stop = 1000, num = 10)]
-# Maximum number of levels in tree
-max_depth = [int(x) for x in np.linspace(1, 15, num = 15)]
-# Create the random grid
-random_grid = {'n_estimators': n_estimators,
-               'max_depth': max_depth}
-
-rf = RandomForestClassifier()
-rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose = 1, n_jobs=-1, random_state=0)
-rf_random.fit(X, y)
-predictions = rf_random.predict(X_test)
-
-print(round(rf_random.score(X, y), 2))
-
-# Create a DataFrame from cv_results_
-cv_results_df = pd.DataFrame(rf_random.cv_results_)
-
-# Drop the specified columns
-cv_results_df = cv_results_df.drop([
-            'mean_fit_time', 
-            'std_fit_time', 
-            'mean_score_time',
-            'std_score_time', 
-            'params', 
-            'split0_test_score', 
-            'split1_test_score', 
-            'split2_test_score', 
-            'std_test_score'],
-            axis=1)
-
-# Sort the DataFrame by rank_test_score
-cv_results_df = cv_results_df.sort_values(by='rank_test_score')
-
-# Print the DataFrame
-print(cv_results_df)
-
-# Create four figures and bar plots
-for column in ['param_n_estimators', 'param_max_depth', 'mean_test_score', 'rank_test_score']:
-    fig = plt.figure()
-    fig.set_size_inches(30, 25)
-    sns.barplot(x=cv_results_df.index, y=column, data=cv_results_df)
-    plt.title(f'Bar plot of {column}')
-    plt.show()
-
-# output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
-# output.to_csv('submission.csv', index=False)
-# print("Your submission was successfully saved!")
